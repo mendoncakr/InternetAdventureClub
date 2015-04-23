3a2017e4f0deb66516366f733dafc80540499155
Kenneth Mendonça <mendonca.kr@gmail.com>
2015-04-23 17:23:36 -0400

    updates add mission to account for error handling when non-unique phone is given

--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from travel.models import Mission, Contact
 from travel.forms import MissionForm, ContactForm
 
@@ -12,16 +12,20 @@ def add_mission(request):
     mission_form = MissionForm(request.POST)
     contact_form = ContactForm(request.POST)
     if mission_form.is_valid() and contact_form.is_valid():
-      contact = Contact.objects.get_or_create(name=request.POST['name'], 
-                        phone=request.POST['phone'], 
-                        email=request.POST['email'])
-      print(contact)
-      contact.save()
+      contact = Contact.objects.get_or_create(phone=request.POST['phone'])
+      if contact[1] == False:
+        contact = contact[0]
+      else:
+        contact = contact[0]
+        contact.name = request.POST['name']
+        contact.email = request.POST['email']
+        contact.save()
       mission = Mission(description=request.POST['description'],
                         anything_else=request.POST['anything_else'])
       mission.contact_id = contact.id
       mission.save()
     else:
+      # Let's render form errors to bootstrap
       print(mission_form.errors)
       print(contact_form.errors)
       return HttpResponse('Form Errors!!')
@@ -31,4 +35,4 @@ def add_mission(request):
     context_dict = {'mission_form' : mission_form,
                     'contact_form' : contact_form}
     return render(request, 'travel/register.html', context_dict)
-  return index(request)
+  return redirect('home')
