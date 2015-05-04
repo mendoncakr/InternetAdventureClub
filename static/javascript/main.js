a84bac060992339c21b9537addc458c34e18bfee
Kenneth Mendonça <mendonca.kr@gmail.com>
2015-05-04 17:44:07 -0400

    we have a mapgs

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
   
-})
+
+  $('#map').ready(function(e){
+    cartodb.createVis('map', 'https://mendoncakr.cartodb.com/api/v2/viz/6f745cd2-f2a5-11e4-a632-0e4fddd5de28/viz.json');
+  });
+});
