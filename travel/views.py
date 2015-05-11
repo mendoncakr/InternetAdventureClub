2e2c20ee9c3e3699a82b9eb6fe4b8a71b2c0fbe7
Kenneth Mendonça <mendonca.kr@gmail.com>
2015-05-11 15:58:26 -0400

    adds working couch page

--- travel/views.py
+++ travel/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from travel.models import Mission, Contact, Address
+from travel.models import Mission, Contact, Address, Couch
 from travel.forms import MissionForm, ContactForm, AddressForm, CouchForm
 
 # Create your views here.
@@ -44,7 +44,7 @@ def add_mission(request):
       coords = mission.geolocate()
       mission.latitude, mission.longitude = coords[0], coords[1]
       mission.save()
-      messages.add_message(request, messages.INFO, 'Your Mission Has Successfully Been Added!')
+      messages.add_message(request, messages.INFO, 'Thanks! Your mission will be added to the list as soon as Chris spell checks it!')
       return redirect('all_missions')
     else:
       print(mission_form.errors)
@@ -62,11 +62,29 @@ def all_missions(request):
 
 def add_couch(request):
   if request.method == "POST":
-    print(request)
+    contact_form, address_form = ContactForm(request.POST), AddressForm(request.POST)
+    if contact_form.is_valid() and address_form.is_valid():
+      contact = Contact.objects.get_or_create(phone=request.POST['phone'])
+      address = Address(city=request.POST['city'], street=request.POST['street'], state=request.POST['state'])
+      address.save()
+      if contact[1] == False: 
+        contact = contact[0]
+      else:
+        contact = contact[0]
+        contact.name = request.POST['name']
+        contact.email = request.POST['email']
+        contact.address = address
+        contact.save()
+      couch = Couch()
+      couch.address = address
+      couch.contact =  contact
+      couch.save()
+      messages.add_message(request, messages.INFO, 'Thanks for the offer! We\'ll let you know if we\'re nearby.')
+      return redirect('all_missions')
   else:
     mission_form, contact_form,address_form = MissionForm(), ContactForm(), AddressForm()
     context_dict = {'mission_form' : mission_form,'contact_form' : contact_form,'address_form' : address_form}
-  return render(request, 'travel/new_couch.html', context_dict)
+    return render(request, 'travel/new_couch.html', context_dict)
 
 
 
