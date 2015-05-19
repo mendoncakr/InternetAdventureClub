8112243d7bce6895cdca80e7ad9785e0112e7885
Kenneth <Kenneth@Kenneths-MacBook-Pro.local>
2015-05-19 17:46:02 -0400

    plz werk

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  // $('#map').ready(function(e){
-  //   cartodb.createVis('map', '//internetadventureclub.cartodb.com/api/v2/viz/617ba774-fd8c-11e4-9e57-0e4fddd5de28/viz.json', {https: true});
-  // });
+  $('#map').ready(function(e){
+    cartodb.createVis('map', '//internetadventureclub.cartodb.com/api/v2/viz/617ba774-fd8c-11e4-9e57-0e4fddd5de28/viz.json', {https: true});
+  });
 
   // Resizes Bottom Buttons
   $(window).resize(function(){
